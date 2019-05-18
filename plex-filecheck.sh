eecd0dee90fbabb7bfe6ebc84a0d8839a7dcbcc8
jkirkcaldy <josh.kirkcaldy@gmail.com>
2019-05-18 15:03:51 +0100

    Update plex-filecheck.sh

--- plex-filecheck.sh
+++ plex-filecheck.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
+echo What directory would you like to check?
+read dir
+
+echo "How long far back do you want to go (in days)"
+
+read days
 
 echo Starting now. If there have been many files added, this may take a long time!
 
-cd "/mnt/plex/TV Shows"
+cd $dir
 
-find . -ctime -5 -type f -iname "*.mkv" ! -iname "*.mp4" ! -iname "*.mov" -exec  ~/scripts/checkfile.py \{\} \; > /var/log/plex-file-check/TV_SHOWS.log
+find . -ctime -$days -type f -iname "*.mkv" ! -iname "*.mp4" ! -iname "*.mov" -exec  ~/scripts/checkfile.py \{\} \; > /var/log/plex-file-check/$dir.log
 
-echo TV Shows have finished. View the log at /var/log/plex-file-check/MOVIES.log
+echo $dir have finished. View the log at /var/log/plex-file-check/$dir.log
 
-cd "/mnt/plex/Movies"
 
-find . -ctime -5 -type f -iname "*.mkv" ! -iname "*.mp4" ! -iname "*.mov" -exec  ~/scripts/checkfile.py \{\} \; > /var/log/plex-file-check/MOVIES.log
-
-echo Movies have finished. View the log at /var/log/plex-file-check/MOVIES.log
